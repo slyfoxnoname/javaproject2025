dd149053e296a4db1a061d8a88dd6b82d28b54b6
Egor Orlik <egororlik27@knu.ua>
2025-12-09 22:26:01 +0200

    fix: Update main class entry point.

--- src/main/java/com/webbot/Main.java
+++ src/main/java/com/webbot/Main.java
@@ -1,32 +1,22 @@
 package com.webbot;
 
-import org.telegram.telegrambots.meta.TelegramBotsApi; // Переконайтеся, що імпорт відповідає назві вашого класу Бота
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import com.webbot.Bot.TelegramBot;
+import com.webbot.Bot.TelegramInterface;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // 1. Ініціалізація API Telegram
-            // Це створює сесію, яка буде слухати оновлення від серверів Telegram
+            // Инициализация API Телеграма
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-
-            // 2. Реєстрація нашого бота
-            // Ми створюємо екземпляр MyBot. У цей момент всередині нього 
-            // також створюється ParserService і готується планувальник.
-            TelegramBot myBot = new TelegramBot();
-            botsApi.registerBot(myBot);
-
-            System.out.println("✅ Бот успішно запущено! Він готовий приймати команди.");
             
-        } catch (TelegramApiException e) {
-            System.err.println("❌ Помилка при запуску бота: " + e.getMessage());
-            e.printStackTrace();
+            // Регистрация нашего бота
+            botsApi.registerBot(new TelegramInterface());
+            
+            System.out.println("Telegram бот успешно запущен!");
         } catch (Exception e) {
-            System.err.println("❌ Непередбачена помилка: " + e.getMessage());
-            e.printStackTrace();
+            e.printStackTrace();    
         }
     }
 }
